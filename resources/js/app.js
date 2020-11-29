f79e1a64f6f6446e3070ddd07ebb8275f6f75498
Ishan Vyas <isvyas@gmail.com>
2020-11-29 15:29:42 +0530

    Add sweetalert to prompt user on success

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,14 @@
 import Vue from 'vue';
 import axios from 'axios';
 import Errors from './helpers/FormErrors.js';
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Vue = Vue;
+
+Vue.use(VueSweetalert2);
 
 const CUSTOMER_SUPPORT = '1';
 
@@ -32,16 +36,27 @@ const app = new Vue({
                 }
             }).then(response => {
                 if (response.data.success) {
-                    this.$notify({
-                        group: 'default',
-                        type: 'success',
-                        text: response.data.message
-                    });
+                    this.$swal(
+                        '',
+                        response.data.message,
+                        'success'
+                    );
 
-                    // Reset form fields
+                    this.resetForm();
                 }
             }).catch(error => {
                 this.errors.add(error.response.data.errors);
+            });
+        },
+        resetForm() {
+            var self = this;
+
+            Object.keys(this.form).forEach(function(key, index) {
+                if (key === 'contact_type') {
+                    self.form[key] = CUSTOMER_SUPPORT;
+                } else {
+                    self.form[key] = '';
+                }
             });
         }
     }
